fc23bb999c5ad59346d56792406129b446195862
Bavoua Tabakem Brunel <angeloferdinand66@gmail.com>
2025-07-14 23:18:17 +0100

    Base Code

--- Moyenne_generale/MoyenneGenerale.java
+++ Moyenne_generale/MoyenneGenerale.java
@@ -6,36 +6,44 @@ public class MoyenneGenerale {
         Scanner scan = new Scanner(System.in).useLocale(Locale.US);
 
         String[]subject;
-        float[] notes, coef;
+        float[] marks, coefs;
+        float sum = 0, totalCoef = 0, average;
 
-        System.out.print("Enter the size of your Tables: ");
+        System.out.print("Number of Subjects: ");
         int size = scan.nextInt();
         scan.nextLine();
         System.out.println();
 
         subject = new String[size];
-        notes = new float[size];
-        coef =  new float[size];
+        marks = new float[size];
+        coefs =  new float[size];
 
         for (int i = 0; i < size; i++) {
-            System.out.print("Enter a subject: ");
+            System.out.print("Name of the subject: ");
             subject[i] = scan.nextLine();
 
-            System.out.print("Enter a number: ");
-            notes[i] = scan.nextFloat();
+            System.out.print("Mark: ");
+            marks[i] = scan.nextFloat();
 
-            System.out.print("Enter another number: ");
-            coef[i] = scan.nextFloat();
+            System.out.print("Coefficient: ");
+            coefs[i] = scan.nextFloat();
             scan.nextLine();
-            
+
             System.out.println();
         }
 
         for (int i = 0; i < size; i++) {
-            System.out.printf("%s\t%.2f\t%.2f\n", subject[i],notes[i],coef[i]);
-            System.out.println();
+            sum += marks[i] * coefs[i];
+            totalCoef += coefs[i];
         }
 
+        average = sum / totalCoef;
+        
+        for (int i = 0; i < size; i++) {
+            System.out.printf("%s\t%.2f\t%.2f\n", subject[i],marks[i],coefs[i]);
+        }
+        System.out.printf("\nAverage: %.2f", average);
+        
         scan.close();
 
 
